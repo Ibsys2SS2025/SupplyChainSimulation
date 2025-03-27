368935edc6a076671df7f26afac05c2f6bd823fe
vinpuch <puvi1011@h-ka.de>
2025-03-27 10:01:32 +0100

    Initial commit

---
+++ src/app/input/page.tsx
@@ -0,0 +1,8 @@
+export default function InputPage() {
+    return (
+        <div className="container mt-4">
+            <h2>Input-Seite</h2>
+            <p>Hier kommt das, was früher im Angular-Input-Modul war.</p>
+        </div>
+    );
+}
